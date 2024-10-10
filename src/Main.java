ddbc5df1dc8c4287eb524dead6394c811f13b8b0
p.manilov <pavman0123456789@gmail.com>
2024-10-11 00:56:45 +0300

    Add test case in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product p1 = new Product("Тесто");
+        Product p2 = new Product("Мука");
+        Product p3 = new Product("Яйца");
+        Product p4 = new Product("Вода");
+        Product p5 = new Product("Пшеница");
+
+        System.out.println(p1.addProduct(p2)); // true
+        System.out.println(p1.addProduct(p3)); // true
+        System.out.println(p1.addProduct(p4)); // true
+        System.out.println(p2.addProduct(p1)); // false
+        System.out.println(p2.addProduct(p5)); // true
+        System.out.println(p5.addProduct(p1)); // false
     }
 }
